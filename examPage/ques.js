47f5f3f748f3237ac0df415786d9150c828f0564
Abdelbadea <Abdo250102002@gmail.com>
2025-10-28 20:35:15 +0300

    adding questions changing part

--- examPage/ques.js
+++ examPage/ques.js
@@ -1,7 +1,40 @@
-let question = {
-    answers :[,,,,] ;
-    correct : 
+let question1 = {
+    ques : "how are you doing?",
+    answers :["good","bad","nice","Stright"] ,
+    correct : 1
+}
+let question2 = {
+    ques : "how many fingers are  in your both hands?",
+    answers :["5","10","15","20"] ,
+    correct : 1
+}
+let question3 = {
+    ques : "Do dogs fly?",
+    answers :["No","Yes","Maybe","IDK"] ,
+    correct : 1
 }
 
-let ques = [question,]
+let ques = [question1,question2,question3];
+
+
+let quesEl = document.querySelector(".question h1")
+
+
+function showQuest(question){
+quesEl.textContent = ques[question-1].ques;
+for(let i = 1; i<=4;i++){
+    let answer = document.querySelector(`.answers .answer-${i}`)
+    answer.textContent = ques[question-1].answers[i-1]
+}
+}
+
+
+let question  = 1 ;
+showQuest(question)
+function next(){
+    question++;
+    if(question<=ques.length)showQuest(question)
+}
+
+
 
