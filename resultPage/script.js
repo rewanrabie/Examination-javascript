807deb946fe73f9be4cc6c9a2a1ff098798c59fc
Abdelbadea <Abdo250102002@gmail.com>
2025-11-03 21:19:27 +0200

    add/score-show

--- resultPage/script.js
+++ resultPage/script.js
@@ -1,6 +1,23 @@
 let mark  = localStorage.getItem("mark")
-let result = document.querySelector("h1")
+let result = document.querySelector("#score")
 
 
 
-result.textContent=mark;
+result.textContent=mark+"%";
+
+  const scoreElement = document.getElementById("score");
+  const scoreValue = mark; 
+        
+  
+  
+  
+  
+  let count = 0;
+  const interval = setInterval(() => {
+    if (count >= scoreValue) clearInterval(interval);
+    else {
+      count++;
+      scoreElement.textContent = count + "%";
+      scoreElement.style.setProperty('--score', count);
+    }
+  }, 30);
